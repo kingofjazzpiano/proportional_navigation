62c8d0b2f12e7bf87a2e9a6d2cc29d72156dc2f0
kingofjazzpiano <mrkingofjazzpiano@gmail.com>
2022-09-09 15:59:50 +0400

    all works again

--- Missile.gd
+++ Missile.gd
@@ -1,4 +1,4 @@
-extends KinematicBody2D
+extends Sprite
 
 onready var debug_line_1 = get_node("/root/Main/DebugPanel/VBoxContainer/Label1")
 onready var debug_line_2 = get_node("/root/Main/DebugPanel/VBoxContainer/Label2")
@@ -10,68 +10,102 @@ onready var debug_line_7 = get_node("/root/Main/DebugPanel/VBoxContainer/Label7"
 onready var debug_line_8 = get_node("/root/Main/DebugPanel/VBoxContainer/Label8")
 onready var fps_label = get_node("/root/Main/DebugPanel/FPS")
 
+onready var navigation = get_node("/root/Main").find_node("Navigation2D")
 onready var player = get_node("/root/Main/Player")
 onready var cross = get_node("/root/Main/Cross")
+onready var timer = $Timer
+onready var ray = $RayCast2D
+
+const RAY_LENGTH = 300.0
+const REACTION_TIME = 0
 
 var velocity: Vector2 = Vector2.ZERO
-var speed: float = 40.0  # Pixels per seond
-var rotation_speed: float = 0.3  # Radians per second
+var max_speed: float = 120.0  # Pixels per seond
+var current_speed: float = max_speed  # Pixels per seond
+var acceleration: float = 0.25
+var max_rotation_speed: float = 0.4  # Radians per second
+var current_rotation_speed: float = max_rotation_speed  # Radians per second
+
+var path: PoolVector2Array = []
+var target_position: PoolVector2Array = []
+var target_velocity: PoolVector2Array = []
+var target_speed: PoolRealArray = []
+
+
+func _ready() -> void:
+	ray.set_cast_to(Vector2(RAY_LENGTH, 0))
+	if REACTION_TIME:
+		timer.start(REACTION_TIME)
 
 
 func _physics_process(delta: float) -> void:
 	fps_label.text = str(round(1.0 / delta))
-	var sight_line: Vector2 = position.direction_to(player.position)
-	var alpha: float = player.velocity.angle_to(-sight_line)
+	if timer.is_stopped() and target_speed.size():
+		target_position.remove(0)
+		target_velocity.remove(0)
+		target_speed.remove(0)
+	target_position.append(player.position)
+	target_velocity.append(player.velocity)
+	target_speed.append(player.speed)
+	var sight_line: Vector2 = position.direction_to(target_position[0])
+	var alpha: float = target_velocity[0].angle_to(-sight_line)
 	
 	var beta: float
 	var tmp: float = 0
-	if speed:
-		tmp = sin(alpha) * player.speed / speed
-
-#	if abs(alpha) < PI * 0.5:
-#		if abs(tmp) > 1.0:
-#			beta = asin(sin(alpha))
-#		else:
-#			beta = asin(tmp)
-#	elif speed < player.speed:
-#		beta = asin(sin(alpha))
-#	else:
-#		beta = asin(tmp)
+	if current_speed:
+		tmp = sin(alpha) * target_speed[0] / current_speed
 	
-	if speed > player.speed or abs(alpha) < PI * 0.5 and abs(tmp) <= 1.0:
+	if current_speed > target_speed[0] or abs(alpha) < PI * 0.5 and abs(tmp) <= 1.0:
 		beta = asin(tmp)
-#		cross.visible = true
 	else:
 		beta = asin(sin(alpha))
-#		cross.visible = false
 
-	if fmod(abs(sight_line.angle() + beta - rotation), PI) >= rotation_speed * delta:
-		rotation = lerp_angle(rotation,
-			sight_line.angle() + beta,
-			abs((rotation_speed * delta) /
-			(Vector2.UP.rotated(rotation).angle_to(Vector2.UP.rotated(sight_line.angle() + beta)))))
-
-#	rotation = sight_line.angle() + beta
-	position += Vector2.RIGHT.rotated(rotation) * speed * delta
-#	velocity = Vector2.RIGHT.rotated(rotation) * speed
-#	velocity = move_and_slide(velocity, Vector2( 0, 0 ), false, 400)
-	
-	# Set cross
 	var gamma: float =  PI - alpha - beta
 	var l: float = 0
 	if alpha:
-		l = speed * sin(gamma) / sin(alpha)
+		l = current_speed * sin(gamma) / sin(alpha)
 	var proportional_coefficient: float = 0
 	if l:
-		proportional_coefficient = position.distance_to(player.position) / l
-	if player.velocity != Vector2.ZERO:
-		if abs(speed * proportional_coefficient) > 1000 or proportional_coefficient == 0:
-			cross.position = position + Vector2.RIGHT.rotated(rotation) * 1000
+		proportional_coefficient = position.distance_to(target_position[0]) / l
+	if target_velocity[0] != Vector2.ZERO:
+		if abs(current_speed * proportional_coefficient) > 1000 or proportional_coefficient == 0:
+			cross.position = position + Vector2.RIGHT.rotated(sight_line.angle() + beta) * 1000
 		else:
-			cross.position = position + Vector2.RIGHT.rotated(rotation) * speed * proportional_coefficient
+			cross.position = position + Vector2.RIGHT.rotated(sight_line.angle() + beta) * current_speed * proportional_coefficient
 	else:
-		cross.position = player.position
+		cross.position = target_position[0]
 
+	update_navigation_path(position, cross.position)
+	
+	var desired_rotation = 0
+	if path.size():
+		desired_rotation = Vector2.RIGHT.rotated(rotation).angle_to(position.direction_to(path[0]))
+	if desired_rotation:
+		rotation = lerp_angle(rotation,
+			position.direction_to(path[0]).angle(),
+			abs((current_rotation_speed * delta) / desired_rotation))
+	position += Vector2.RIGHT.rotated(rotation) * current_speed * delta
+	
+	
+	if ray.is_colliding():
+		var estimated_speed: float = position.distance_to(ray.get_collision_point()) * max_speed / RAY_LENGTH
+		if estimated_speed and estimated_speed < current_speed:
+			current_speed = estimated_speed
+		else:
+			current_speed += acceleration
+	else:
+		current_speed += acceleration * sign(sight_line.dot(Vector2.RIGHT.rotated(rotation)))
+	current_speed = clamp(current_speed, 0, max_speed)
+	
 	debug_line_1.text = "alpha: " + str(rad2deg(alpha))
 	debug_line_2.text = "beta: " + str(rad2deg(beta))
 	debug_line_3.text = "cross.position: " + str(cross.position)
+	debug_line_4.text = "ray.is_colliding(): " + str(ray.is_colliding())
+	debug_line_5.text = "current_speed: " + str(current_speed)
+	debug_line_6.text = "sign(sight_line.dot(Vector2.RIGHT.rotated(rotation))): " + str(sign(sight_line.dot(Vector2.RIGHT.rotated(rotation))))
+
+
+func update_navigation_path(start_position, end_position):
+	path = navigation.get_simple_path(start_position, end_position, true)
+	if path.size():
+		path.remove(0)
--- player.gd
+++ player.gd
@@ -1,31 +1,25 @@
 extends Sprite
 
 var velocity: Vector2 = Vector2.ZERO
-var speed: float = 100.0
-onready var target_point: Vector2 = global_position
+var max_speed: float = 100.0
+var speed: float = 0.0
+var acceleration: float = 0.5
 
 
 func _physics_process(delta: float) -> void:
 	velocity = Vector2.ZERO
 	get_input(delta)
 	
-	velocity = velocity.rotated(rotation)
+	velocity = Vector2.RIGHT.rotated(rotation)
 	position += velocity * speed * delta
-	
-#	if position.distance_to(target_point) > speed * delta:
-#		velocity = position.direction_to(target_point)
-#	position += velocity * speed * delta
-#	rotation = velocity.angle()
-	
+
 
 func get_input(delta: float) -> void:
-	if Input.is_action_just_pressed("left_mouse"):
-		target_point = get_global_mouse_position()
-
 	if Input.is_action_pressed("w"):
-		velocity = Vector2.RIGHT
+		speed += acceleration
 	elif Input.is_action_pressed("s"):
-		velocity = Vector2.LEFT
+		speed -= acceleration
+	speed = clamp(speed, 0, +max_speed)
 
 	if Input.is_action_pressed("a"):
 		rotation -= (PI / 6.0) * delta
